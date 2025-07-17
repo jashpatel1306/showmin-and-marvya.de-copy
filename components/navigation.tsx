b5494b68f9f8189c7de77e789c23ebffec465d4d
jashpatel1306 <jashpatel1306@gmail.com>
2025-07-17 17:52:04 +0530

    comment

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -76,7 +76,7 @@ export function Navigation() {
               ))}
             </div>
 
-           
+            {/* Contact Button */}
             <motion.div
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
