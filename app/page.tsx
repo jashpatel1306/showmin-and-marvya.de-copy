fba90210c417584aa07a874e9734fd0f8ca2f5aa
jashpatel1306 <jashpatel1306@gmail.com>
2025-09-22 17:12:03 +0530

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -1931,7 +1931,6 @@ export default function HomePage() {
       </section>
 
 
-      {/* Team Section */}
       <section className="py-20 bg-black">
         <div className="max-w-7xl mx-auto px-6">
           <motion.div
