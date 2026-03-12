6f6677c02c1d1db567860c78473cc52cd7bfaf59
jashpatel1306 <jashpatel1306@gmail.com>
2026-03-12 18:04:58 +0530

    add new page.

--- src/pages/WebsiteDesign.tsx
+++ src/pages/WebsiteDesign.tsx
@@ -5,17 +5,18 @@ import Process from "@/components/Process";
 import ServiceSection from "@/components/WebsiteDesign/ServiceSection";
 import FaqSection from "@/components/WebsiteDesign/FaqSection";
 import HeroSection from "@/components/WebsiteDesign/HeroSection";
+import PopularServices from "@/components/WebsiteDesign/PopularServices";
 
 const WebsiteDesign = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
       <HeroSection />
-    <ServiceSection />
-    <FaqSection />
+      <ServiceSection />
+      <FaqSection />
+      <PopularServices />
       <Process />
       <CTA />
-
       <Footer />
     </div>
   );
